27a97453fa789acb6e2e833e8341c363a54923c2
Lutica_CANARD <presan100@gmail.com>
2023-12-09 23:37:25 +0900

    ✅ ADD : 가로 준비

--- mainVer.js
+++ mainVer.js
@@ -13,33 +13,35 @@ const count_row = 1;
 const count_col = 2;
 
 // 아틀라싱 툴임. 다만, 유니티 내에서 편집가능하면 패스해도됨.
-
-const canvas = new fabric.StaticCanvas(null, {width: size_width*count_col, height: size_height*count_row})
-const promiseArray = []
-for(let row=0;row<count_row;row++){
-  let callRow = row+1;
-  const file = await fs.readdirSync(`./origin/canvas${callRow}`);
-  for(let col=0;col<count_col;col++){
-    const p = new Promise((resolve) => {
-      fabric.Image.fromURL('file://'+__dirname+`/origin/canvas${callRow}/${file[col]}`,(oImg)=>{
-        oImg.set({ 
-          id : 'image_'+row+'_'+col,
-          left:col*size_width,
-          top:row*size_height,
-          opacity: 1,      
+for(let stream=3;stream<5;stream++){
+  const canvas = new fabric.StaticCanvas(null, {width: size_width*count_col, height: size_height*count_row})
+  const promiseArray = []
+  for(let row=0;row<count_row;row++){
+    let callRow = row+1;
+    const file = await fs.readdirSync(`./origin/${stream}/canvas${callRow}`);
+    for(let col=0;col<count_col;col++){
+      const p = new Promise((resolve) => {
+        fabric.Image.fromURL('file://'+__dirname+`/origin/${stream}/canvas${callRow}/${file[col]}`,(oImg)=>{
+          oImg.set({ 
+            id : 'image_'+row+'_'+col,
+            left:col*size_width,
+            top:row*size_height,
+            opacity: 1,      
+          })
+          oImg.scaleToHeight(size_height);
+          oImg.scaleToWidth(size_width);
+          canvas.add(oImg);
+          resolve();
         })
-        oImg.scaleToHeight(size_height);
-        oImg.scaleToWidth(size_width);
-        canvas.add(oImg);
-        resolve();
-      })
-    });
-    promiseArray.push(p);
+      });
+      promiseArray.push(p);
+    }
   }
-}
-Promise.all(promiseArray)
-  .then(() => {
-    canvas.renderAll();
-    canvas.createPNGStream().pipe(fs.createWriteStream("./docs/output.png")) 
-    console.log('END;');
+  Promise.all(promiseArray)
+    .then(() => {
+      canvas.renderAll();
+      canvas.createPNGStream().pipe(fs.createWriteStream(`./docs/output${stream}.png`)) 
+      console.log('END;');
     })
+}
+
